289da7652f7e1d01c0cfea559c7e2c99b955ac45
Snowplou <snowplou01@gmail.com>
2025-02-15 12:21:52 -0800

    started working more on the transformer

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchtext.data.utils import get_tokenizer
 
+max_token_length = 9
+
 train_data = [
     "this is a sentence",
     "two words",
@@ -12,5 +14,39 @@ train_data = [
     "the quick brown fox jumps over the lazy dog",
 ]
 
+# Create a tokenizer based off of the training data
 tokenizer = get_tokenizer("basic_english")
-train_data = [tokenizer(item) for item in train_data]
+vocab = set()
+for line in train_data:
+    vocab.update(tokenizer(line))
+vocab = list(vocab)
+
+# Create a mapping from words to indices
+word_to_idx = {word: i for i, word in enumerate(vocab)}
+
+# Special tokens
+word_to_idx["<PAD>"] = len(word_to_idx)
+vocab.append("<PAD>")
+word_to_idx["<UNK>"] = len(word_to_idx)
+vocab.append("<UNK>")
+
+def encode(line):
+    tokens = tokenizer(line)
+    tokens = [word_to_idx.get(token, word_to_idx["<UNK>"]) for token in tokens]
+    tokens += [word_to_idx["<PAD>"]] * (max_token_length - len(tokens))
+    return tokens
+
+def decode(tokens):
+    return [vocab[token] for token in tokens]
+
+training_data = [encode(line) for line in train_data]
+
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.embedding = nn.Embedding(len(vocab), 10)
+#         self.f1 = nn.Linear(len(vocab) * 10, 100)
+#         self.relu = nn.ReLU()
+
+#     def forward(self, x):
+#         return self.fc(x)
