754e5bdeed81716085761542089b68d96a19d3b2
Snowplou <snowplou01@gmail.com>
2025-02-16 09:14:49 -0800

    trained on 1000 wiki samples

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ import json
 
 max_token_length = 20
 
-train_data = json.load(open("training_data.json", "r"))[:100]
+train_data = json.load(open("training_data.json", "r"))[:1000]
 
 # Create a tokenizer based off of the training data
 tokenizer = get_tokenizer("basic_english")
