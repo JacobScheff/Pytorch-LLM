3ef1e5f52da4d3bf4a0af576d86762c7e642110e
Snowplou <snowplou01@gmail.com>
2025-02-16 11:10:50 -0800

    added gpt2 tokenizer to data.py

--- data.py
+++ data.py
@@ -4,45 +4,30 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchtext.data.utils import get_tokenizer
 import json
+from transformers import GPT2Tokenizer
 
 max_token_length = 20
 
 # Load the training data
 print("Loading training data...")
-train_data = json.load(open("training_data.json", "r", encoding="utf-8"))
+train_data = json.load(open("training_data.json", "r", encoding="utf-8"))[:10]
 
-# Create a tokenizer based off of the training data
-tokenizer = get_tokenizer("basic_english")
-vocab = set()
-for line in train_data:
-    vocab.update(tokenizer(line))
-vocab = sorted(list(vocab))
-print("vocab size: " + str(len(vocab)))
-
-# Create a mapping from words to indices
-word_to_id = {word: i for i, word in enumerate(vocab)}
-
-# Special tokens
-word_to_id["<PAD>"] = len(word_to_id)
-vocab.append("<PAD>")
-word_to_id["<UNK>"] = len(word_to_id)
-vocab.append("<UNK>")
-
-# Save word_to_id and vocab
-torch.save(word_to_id, "word_to_id.pth")
-torch.save(vocab, "vocab.pth")
+# Load the tokenizer
+print("Loading tokenizer...")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+tokenizer.add_special_tokens({"pad_token": "<PAD>"}) # Add a PAD token
 
 def encode(line, truncate=True):
-    tokens = tokenizer(line)
-    tokens = [word_to_id.get(token, word_to_id["<UNK>"]) for token in tokens]
-    tokens += [word_to_id["<PAD>"]] * (max_token_length - len(tokens))
+    tokens = tokenizer.tokenize(line)
+    tokens = tokenizer.convert_tokens_to_ids(tokens)
+    tokens += [tokenizer.pad_token_id] * (max_token_length - len(tokens))
     if truncate and len(tokens) > max_token_length:
         # Remove the first tokens
         tokens = tokens[-max_token_length:]
     return tokens
 
 def decode(tokens):
-    return [vocab[token] for token in tokens]
+    return tokenizer.convert_ids_to_tokens(tokens)
 
 # Encode the training data
 print("Encoding training data...")
@@ -53,7 +38,7 @@ print("Creating X and y...")
 X, y = [], []
 for line in encoded_train_data:
     for i in range(1, len(line)):
-        X.append((line[:i] + [word_to_id["<PAD>"]] * max(max_token_length - i, 0))[-max_token_length:])
+        X.append((line[:i] + [tokenizer.pad_token_id] * max(max_token_length - i, 0))[-max_token_length:])
         y.append([line[i]])
 
 # Convert to tensors
