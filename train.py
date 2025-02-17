d0b8ae79bd68beb22a3c75118da1c53c30fd147b
Snowplou <snowplou01@gmail.com>
2025-02-17 14:26:38 -0800

    it cant generalize

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from transformers import GPT2Tokenizer
 torch.manual_seed(0) # Set seed for reproducibility
 
 max_token_length = 20
-batch_size = 64
+batch_size = 256
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
