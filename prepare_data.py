d40e08e0419e9739d27c249c2a98e04bea11394c
Snowplou <snowplou01@gmail.com>
2025-02-26 18:14:58 -0800

    combined all of wiki dataset into one json file

--- prepare_data.py
+++ prepare_data.py
@@ -2,16 +2,21 @@ import pandas as pd
 import json
 from tqdm import tqdm
 
-df = pd.read_parquet('wiki_data.parquet')
-# Write the text column to a json file. the output should look like this: ["this is a sentence", "this is another sentence", ...]
-text = df['text']
-output = []
-for i in tqdm(range(len(text))):
-    out = text[i].split('\n')
-    for j in range(len(out)):
-        if len(out[j].split(" ")) > 10: # Only include entries with more than 10 words
-            output.append(out[j])
-            break # Only include one entry per wikipedia page to not train too much on one subject
+result = []
+
+for i in range(41):
+    df = pd.read_parquet('wiki_data.parquet')
+    # Write the text column to a json file. the output should look like this: ["this is a sentence", "this is another sentence", ...]
+    text = df['text']
+    output = []
+    for i in tqdm(range(len(text))):
+        out = text[i].split('\n')
+        for j in range(len(out)):
+            if len(out[j].split(" ")) > 10: # Only include entries with more than 10 words
+                output.append(out[j])
+                break # Only include one entry per wikipedia page to not train too much on one subject
+    for i in range(len(output)):
+        result.append(output[i])
 
 with open('wiki_training_data.json', 'w', encoding='utf-8') as f:
-    json.dump(output, f, ensure_ascii=False, indent=4)
+    json.dump(result, f, ensure_ascii=False, indent=4)
